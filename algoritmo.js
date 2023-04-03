4bd2cb399cf6ee563541a5eb9f0a8daf01cf05a4
Alan <alansarchman@live.com.ar>
2023-04-03 19:16:12 -0300

    modifacion de la practica

--- algoritmo.js
+++ algoritmo.js
@@ -4,6 +4,8 @@ document.querySelector("body").innerHTML += "<h2> hola 2 </h2>"; */
 
 let variable = "vacio";
 variable = prompt("Ingresa un color");
+document.write ("<h1> comienzo la serie<h1>");
 document.write(" <h2> Color ingreasdo es: " + variable+"<h2>");
 document.querySelector("body").innerHTML += "<h3> Color ingreasdo es: " + variable+"<h3>";
 console.log("Color ingreasdo es:"  + variable);
+
