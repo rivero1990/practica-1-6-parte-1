a9a737a3823d64bfcc4dbbd1991eba06f76aab1d
Alan <alansarchman@live.com.ar>
2023-04-03 20:00:25 -0300

    practica 1-6 parte 2

--- algoritmo.js
+++ algoritmo.js
@@ -2,10 +2,27 @@
 console.log ("hola 2");
 document.querySelector("body").innerHTML += "<h2> hola 2 </h2>"; */
 
-let variable = "vacio";
-variable = prompt("Ingresa un color");
-document.write ("<h1> comienzo la serie<h1>");
-document.write(" <h2> Color ingreasdo es: " + variable+"<h2>");
-document.querySelector("body").innerHTML += "<h3> Color ingreasdo es: " + variable+"<h3>";
-console.log("Color ingreasdo es:"  + variable);
+let caracterIngresado = "a";
+let stringIngresado = "vacio";
+let enteroIngresado = 123;
+let flotanteIngresado = 123.8;
+let booleanoIngresado = false;
+
+document.write ("<h1> Ahora pido todos los datos <h1>");
+
+caracterIngresado = prompt("Ingresa un caracter");
+stringIngresado = prompt("Ingresa un string");
+enteroIngresado= prompt("Ingresa un entero");
+flotanteIngresado = prompt("Ingresa un flotante");
+booleanoIngresado = prompt("Ingresa un booleano");
+
+document.write ("<h1> Ahora imprimo todo<h1>");
+
+document.write(" <h2> Caracter ingresado es: " + caracterIngresado +"<h2>");
+document.write(" <h2> String ingresado es: " + stringIngresado +"<h2>");
+document.write(" <h2> Entero ingresado es: " + enteroIngresado +"<h2>");
+document.write(" <h2> Flotante ingresado es: " + flotanteIngresado +"<h2>");
+document.write(" <h2> Booleano ingresado es: " + booleanoIngresado +"<h2>");
+
+;
 
