6be33534ec697afd879da2341f4e7388c29da48a
Alan <alansarchman@live.com.ar>
2023-04-03 19:05:17 -0300

    pratica 1-6 parte 1

---
+++ algoritmo.js
@@ -0,0 +1,9 @@
+/* document.write("hola");
+console.log ("hola 2");
+document.querySelector("body").innerHTML += "<h2> hola 2 </h2>"; */
+
+let variable = "vacio";
+variable = prompt("Ingresa un color");
+document.write(" <h2> Color ingreasdo es: " + variable+"<h2>");
+document.querySelector("body").innerHTML += "<h3> Color ingreasdo es: " + variable+"<h3>";
+console.log("Color ingreasdo es:"  + variable);
